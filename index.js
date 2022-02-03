8ba994ab05f91476b633063a5c948eb700ae22ed
Jessica Newman <jess.newman19@gmail.com>
2022-02-02 21:32:50 -0500

    commit tree fe368d3aa0d397dbc6ba0d6f8cb13ae202f733bf

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Follow along with the examples here
+// function doNothing() {}
+// function sayHello () {
+//     console.log("Hello!")
+// }
+// sayHello();
+// function sayHelloToGuadalupe() {
+//     console.log("Hello, Guadalupe!")
+// }; 
+// sayHelloToGuadalupe(); 
+// function sayHelloToLiz () {
+//     console.log("Hello, Liz")
+// }; 
+// sayHelloToLiz(); 
+// function sayHelloToSaimp() {
+//     console.log("Hello, Saimp!")
+// }; 
+// sayHelloToSaimp();
+
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}!`);
+// }; 
+// sayHelloTo("Guadalupe"); 
+// sayHelloTo("Jane"); 
+// sayHelloTo("R2-D2"); 
+// sayHelloTo(1); 
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+// }; 
+// say ("Goodbye", "Julio"); 
+
+// function say (greeting, firstName) { 
+//     console.log ("firstName: ", firstName); 
+//     console.log ("greeting: ", firstName); 
+//     console.log(`${greeting}, ${firstName}!`);
+// }; 
+// say ("Julio", "hello"); 
+
+// function add(x, y) { 
+//     return x + y;
+// }
+// console.log(add(80,9000));
+
+// function say(greeting, firstName) { 
+//     return `${greeting}, ${firstName}!`;
+// }
+
+// console.log(say("Hello", "Liz"));
+
+function say(greeting, firstName) { 
+    console.log("I was called");
+    return `${greeting}, ${firstName}!`; 
+}
+
+console.log(say("Howdy", "partner"));
